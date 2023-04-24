41e1c5116a93b1f5484977319e7f89950efaa56e
Ali Saadat <alisaadt129@gmail.com>
2023-04-24 14:31:55 +0430

    navbar is 100% ok and responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,62 @@
 import React from 'react';
 
-// font awsem icons 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-
+// bootstrap   
+import 'bootstrap/dist/css/bootstrap.css';
 import { Cart4 } from 'react-bootstrap-icons';
+import {Heart} from 'react-bootstrap-icons';
+import {List} from 'react-bootstrap-icons';
+import {ChevronUp} from 'react-bootstrap-icons';
+// css
+import Styles from '../Style/Navbar.module.css';
 
 const Navbar = () => {
+
+
+    const show_nav = (situation) =>{
+        const nav1 = document.querySelector('.nav1');
+        const nav2 = document.querySelector('.nav2');
+        const showbutton = document.querySelector('.button1');
+        const hidebutton = document.querySelector('.button2');
+
+        if(situation=="show"){
+            nav1.style.display='flex'
+            nav2.style.display='flex'
+            showbutton.style.display='none'
+            hidebutton.style.display='block'
+        }
+        else{
+            nav1.style.display='none'
+            nav2.style.display='none'
+            showbutton.style.display='block'
+            hidebutton.style.display='none'
+        }
+    }
+
+
     return (
-        <div>
-            <div>
-                <ul>
-                    <li><h2>ShopLand</h2></li>
+        <div className={`container-fluid ${Styles.navbar}`}>
+            <div className={`row ${Styles.navbar_controller}`}>
+                <h2 className={`col-10 col-lg-1`}>ShopLand</h2>
+                <ul className={`col-lg-6 nav1  ${Styles.navbar_part}`}>
                     <li><a>Home</a></li>
                     <li><a>Products</a></li>
+                    <li><a>call</a></li>
                     <li><a>About us</a></li>
 
                 </ul>
-            </div>
 
-            <div>
-                 <ul>
-                  
-                    <li><a><Cart4></Cart4></a></li>
+                 <ul className={ `col-lg-4 nav2 ${Styles.cart_part}`}>
+                    <li><a className={`${Styles.icon}`}><Heart></Heart></a><span>0</span></li>
+                    <li><a className={`${Styles.icon}`}><Cart4></Cart4></a><span>0</span></li>
                     <li><a>Log in</a></li>
 
                 </ul>
+                <h1 className={`col-1 button1 ${Styles.toggle_button}`}  onClick={() => show_nav("show")}><List /></h1>
+                <h1 className={`col-1 button2 ${Styles.toggle_button2}`}  onClick={() => show_nav("hide")}><ChevronUp /></h1>
+
             </div>
+
+            
         </div>
     );
 };
