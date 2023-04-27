b8998921e2a82919e940ae24ff9b78a844f495ee
Ali Saadat <alisaadt129@gmail.com>
2023-04-27 15:43:08 +0430

    contineu project . navbar is ok . goin to make main page

--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-// test \
+// context 
 import { useContext } from 'react';
 import {Contextprovider} from '../context/Context'
+// css
+import Styles from "../Style/Mainpage.module.css"
+
+// img
+import pictitle from "../picture/rsz_1collection_stylish_young_women_dressed_vector_image_on_vectorstock.jpg"
 
 const Mainpage = () => {
 
@@ -10,8 +15,11 @@ const Mainpage = () => {
 
 
     return (
-        <div>
-            
+        <div className={`container-fluid p-3`}>
+            <div className={`row ${Styles.scroll_pic}`}>
+                <img src={pictitle}></img>
+                <h5>WELCOME TO SHOPLAND</h5>
+            </div>
         </div>
     );
 };
