2c21fd8082a6bdda4e0f2095e13576c494e049bd
Ali Saadat <alisaadt129@gmail.com>
2023-05-05 18:37:04 +0330

    contineu . wprk on card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+// bootstrap   
+import 'bootstrap/dist/css/bootstrap.css';
+import {Heart} from 'react-bootstrap-icons';
+import {FilePlus} from 'react-bootstrap-icons';
+import {FileMinus} from 'react-bootstrap-icons';
+
 
 // css
 import Styles from "../Style/Card.module.css"
@@ -14,10 +20,15 @@ const Card = ({Data}) => {
             <img alt={Data.id} src={Data.image} className={Styles.imageCard}></img>
 
             </div>
-            <div>
+            <div className={`${Styles.info_part}`}>
                 <h3>{Shorter(Data.title)}</h3>
-                <h3>{Data.price} $</h3>
-                
+                <h4>{Data.price} $</h4>
+                <div className={`${Styles.footer_Card}`}>
+                    <Heart /> 
+                    <FilePlus />
+                    <FileMinus />
+                    <button className={`${Styles.buy_Button}`}>Buy</button>
+                </div>
             </div>
         </div>
     );
